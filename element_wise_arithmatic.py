0296cbe0c3d3f8e559f00ce88122a0d9368b12b1
subhashadireddy <subhashadireddy@gmail.com>
2026-03-24 13:45:09 +0530

    added new code

---
+++ element_wise_arithmatic.py
@@ -0,0 +1,10 @@
+import numpy as np
+array = np.array([1,2,3])
+array1 = np.array([4,5,6])
+
+print(array + array1)
+print(array - array1)
+print(array * array1)
+print(array / array1)
+print(array ** array1)
+print(array // array1)
--- scalar_vectorized_math_func.py
+++ scalar_vectorized_math_func.py
@@ -1,3 +1,4 @@
 import numpy as np
 radii = np.array([1,2,3])
-print(np.pi * radii **2)
+print(np.pi * radii **2) ## area of the circle of given radii
+
