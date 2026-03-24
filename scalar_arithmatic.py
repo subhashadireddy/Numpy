8be436b30b05b3a0d005681d3416ee2b9f78350e
subhashadireddy <subhashadireddy@gmail.com>
2026-03-24 13:13:35 +0530

    added new code

---
+++ scalar_arithmatic.py
@@ -0,0 +1,10 @@
+import numpy as np 
+#Scalar Arithmatics....... perform on linear structures on single values... 
+array = np.array([1,2,3])
+print(array+1)
+print(array-1)
+print(array*2)
+print(array//2)
+print(array **2)
+print(array/2)
+
