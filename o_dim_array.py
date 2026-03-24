af46cff7f484c8987532e9887766a556625080d8
subhashadireddy <subhashadireddy@gmail.com>
2026-03-24 12:46:13 +0530

    added new code

--- o_dim_array.py
+++ o_dim_array.py
@@ -2,3 +2,14 @@ import numpy as np
 array = np.array(['a','b','c'])
 print(array.ndim)
 print(array)
+
+#2d array
+array1 = np.array([[1,2,3],[2,5,6],[8,3,6]])
+print(array1)
+print(array1.ndim)
+
+
+#3d array
+array2 = np.array([[[1,2,3],[2,5,6],[8,3,6]],[[1,2,3],[2,5,6],[8,3,6]],[[1,2,3],[2,5,6],[8,3,6]]])
+print(array2)
+print(array2.ndim)
